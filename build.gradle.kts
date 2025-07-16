c3f8aab2bd4d5e327e72ffe606a4210464976e3b
Hwa_Baeg_ <26338400+xodid8881@users.noreply.github.com>
2025-07-17 04:33:25 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.hwabeag"
@@ -7,33 +8,34 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.purpurmc.org/snapshots")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://jitpack.io")
+    maven("https://repo.opencollab.dev/main/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib"))
+
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
-
-repositories {
-    mavenCentral()
-    maven(url = "https://repo.purpurmc.org/snapshots")
-    maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    maven(url = "https://jitpack.io")
-    maven(url = "https://repo.opencollab.dev/main/")
-}
-
-dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.github.LoneDev6:API-ItemsAdder:3.6.3-beta-14")
     compileOnly("org.geysermc.floodgate:api:2.2.2-SNAPSHOT")
     implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
 }
