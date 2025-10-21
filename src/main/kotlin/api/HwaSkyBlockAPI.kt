a0ff628a3c730c0fb6a2f3c74e695dd7f9a57673
Hwa_Baeg_ <26338400+xodid8881@users.noreply.github.com>
2025-10-21 23:52:26 +0900

    Delete src/main directory

--- src/main/kotlin/api/HwaSkyBlockAPI.kt
+++
@@ -1,9 +0,0 @@
-package org.hwabaeg.hwaskyblock.api
-
-import org.bukkit.entity.Player
-
-interface HwaSkyBlockAPI {
-    fun hasIsland(player: Player): Boolean
-    fun hasOwner(player: Player, island_number: Int): Boolean
-    fun upgradeIsland(player: Player, island_number: Int, plus_size: Int)
-}
